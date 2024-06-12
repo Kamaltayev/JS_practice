af2c25147de0105282a9cab91abd8f3c1f932515
Sam <kamaltaevs@gmail.com>
2024-06-12 16:58:38 +0600

    practice__js3

--- script.js
+++ script.js
@@ -19,9 +19,28 @@
         'logan': '8.1'
     }
 
+    
 Проверить, чтобы все работало без ошибок в консоли */
 
-"use strict";
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privet: false,
+// };
+
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//   b = prompt("На сколько оцените его?", ""),
+//   c = prompt("Один из последних просмотренных фильмов?", ""),
+//   d = prompt("На сколько оцените его?", "");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
 
 // let i = 2;
 // while (i <= 16) {
@@ -67,17 +86,85 @@
 //   return result;
 // }
 
-const lines = 5;
-let result = "";
+/* Задание на урок:
 
-for (let i = 0; i <= lines; i++) {
-  for (let j = 0; j < lines - i; j++) {
-    result += " ";
-  }
-  for (let j = 0; j < i * 2 + 1; j++) {
-    result += "*";
-  }
-  result += "\n";
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами  0*/
+
+("use strict");
+
+const numberOfFilms = +prompt("Сколько раз вы полировали шляпу");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privet: false,
+};
+
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert("Вы классический пидорас");
+} else if (personalMovieDB.count > 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
 }
 
-console.log(result);
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt("Один из последних просмотренных фильмов?", ""),
+//     b = prompt("На сколько оцените его?", "");
+
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("done");
+//   } else {
+//     console.log("error");
+//     i--;
+//   }
+// }
+
+let i = 0;
+while (i < 2) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+  i++;
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("everything is ok!");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+
+console.log(personalMovieDB);
+
+// Код возьмите из предыдущего домашнего задания
+
+// const lines = 5;
+// let result = "";
+
+// for (let i = 0; i <= lines; i++) {
+//   for (let j = 0; j < lines - i; j++) {
+//     result += " ";
+//   }
+//   for (let j = 0; j < i * 2 + 1; j++) {
+//     result += "*";
+//   }
+//   result += "\n";
+// }
+
+// console.log(result);
